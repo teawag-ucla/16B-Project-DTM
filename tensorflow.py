d54115307be2716f9b9482d1bc33361039c40128
dcow29 <dcowlagi@g.ucla.edu>
2025-12-03 15:39:19 -0800

    Add files via upload

---
+++ tensorflow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  3 15:10:12 2025
+
+@author: diyacowlagi
+"""
+
+import string
+import tensorflow as tf
+import re
+
+def standardization(input_data):
+    # convert to lowercase
+    lowercase = tf.strings.lower(input_data)
+    # remove punctuation
+    no_punctuation = tf.strings.regex_replace(lowercase,
+                                  '[%s]' % re.escape(string.punctuation), '')
+    return no_punctuation
+
+def vectorize_headline(text, label, vectorize_layer1):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer1(text), [label]
