7cba08da0aa263792f97ce35ac6ec3dc181113a7
dcow29 <dcowlagi@g.ucla.edu>
2025-12-09 21:02:26 -0800

    Update tensorflow.py

--- tensorflow.py
+++ tensorflow.py
@@ -11,6 +11,15 @@ import tensorflow as tf
 import re
 
 def standardization(input_data):
+    """
+    Standardize text input by lowercasing and removing punctuation.
+
+    Paramters:
+    input_data (tf.Tensor or str): the text to be standardized
+
+    Returns:
+    string tensor with all characters lowercased and punctuation removed
+    """
     # convert to lowercase
     lowercase = tf.strings.lower(input_data)
     # remove punctuation
@@ -19,5 +28,16 @@ def standardization(input_data):
     return no_punctuation
 
 def vectorize_headline(text, label, vectorize_layer1):
+    """
+    Vectorize a headline and pair it with its corresponding label.
+
+    Paramters:
+    text (tf.Tensor or str): The headline to be vectorized
+    label (tf.Tensor or int): The label associated 
+    vectorize_layer1 (tf.keras.layers.TextVectorization): A fitted TextVectorization layer used to transform the headline into vectors
+
+    Returns:
+    string tensor with all characters lowercased and punctuation removed
+    """
     text = tf.expand_dims(text, -1)
     return vectorize_layer1(text), [label]
